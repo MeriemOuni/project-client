afb782527c6003fd0f1a5ff46b07546c13851816
meriem <elounimeriem@gmail.com>
2024-07-08 10:58:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import Error from "./Pages/Error";
+import Navigation from "./Components/Navigation";
+import { useDispatch } from "react-redux";
+import { current } from "./Redux/Actions/user";
+import Product from "./Pages/Product";
+import AddProduct from "./Pages/AddProduct";
+import Edit from "./Pages/Edit";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (localStorage.getItem("token")) {
+			dispatch(current());
+		}
+	}, [dispatch]);
+
+	return (
+		<div className='App'>
+			<Navigation />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/product' element={<Product />} />
+				<Route path='/edit/:id' element={<Edit />} />
+				<Route path='/addproduct' element={<AddProduct />} />
+				<Route path='/*' element={<Error />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
